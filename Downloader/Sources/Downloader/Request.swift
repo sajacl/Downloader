ad4b8a700aeb970bd7f37452eeb33586374c7fe5
Sajad Vishkai <sajaclvishkai@gmail.com>
2025-06-26 14:52:20 +0330

    Update Request.swift

--- Downloader/Sources/Downloader/Request.swift
+++ Downloader/Sources/Downloader/Request.swift
@@ -1,5 +1,26 @@
 import Foundation
 
 extension Downloader {
-    struct Request {}
+    /// Object that describes a request for download a resource.
+    public struct Request {
+        /// URL of the resource.
+        public let url: URL
+
+        /// Optional destination for the resource.
+        public let destination: URL?
+
+        public init(url: URL, destination: URL?) {
+            self.url = url
+            self.destination = destination
+        }
+
+        public init?(_ urlString: String, destination: URL?) {
+            guard let url = URL(string: urlString) else {
+                return nil
+            }
+
+            self.url = url
+            self.destination = destination
+        }
+    }
 }
