ad5371dd22133906c354beef47a5cd0edc62b33f
Sajad Vishkai <sajaclvishkai@gmail.com>
2025-07-02 12:39:47 +0330

    Create DefaultDownloadManager.swift

---
+++ Downloader/Sources/Downloader/Manager/DefaultDownloadManager.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+extension Downloader {
+    /// Default download manager, that will be used for simple tasks.
+    static let `default` = Manager(name: "Default")
+    
+    /// Register's requests on default download manager.
+    public static func register(
+        _ request: Request,
+        intercepting completionHandler: ((URL?, URLResponse?, (any Error)?) -> Void)? = nil
+    ) {
+        `default`.register(request, intercepting: completionHandler)
+    }
+}
