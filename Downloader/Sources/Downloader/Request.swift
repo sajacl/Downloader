4fa1228bd6254a5291817c9457fd01596a3cae59
Sajad Vishkai <sajaclvishkai@gmail.com>
2025-07-02 10:54:12 +0330

    Redefine `Request`.

--- Downloader/Sources/Downloader/Request.swift
+++ Downloader/Sources/Downloader/Request.swift
@@ -2,44 +2,24 @@ import Foundation
 
 extension Downloader {
     /// Object that describes a request for download a resource.
-    public struct Request {
-        // Kind? TASK?!
-        public enum Kind: Hashable, Sendable {
-            case foreground(isEphemeral: Bool)
-
-            case background(Background)
-
-            public static let `default`: Kind = .background(
-                Background(
-                    identifier: Bundle.main.bundleIdentifier!,
-                    isDiscretionary: false,
-                    sessionSendsLaunchEvents: true
-                )
-            )
-
-            public struct Background: Hashable, Sendable {
-                /// <#Description#>
-                public let identifier: String
-
-                /// A Boolean flag that determines whether background tasks can be scheduled at the discretion of the system for optimal performance.
-                /// When set to true, the system schedules tasks at its discretion to optimise power and performance.
-                /// (e.g., deferring tasks when the device is low on battery or the network is under heavy load).
-                public var isDiscretionary: Bool
-
-                /// A Boolean flag that indicates whether the app should be resumed or launched in the background when transfers finish.
-                /// Allows the system to relaunch your app in the background when a background task completes or requires your app to handle an event.
-                public var sessionSendsLaunchEvents: Bool
-            }
-        }
-
+    public struct Request: Equatable, Hashable,
+                           Sendable,
+                           CustomStringConvertible, CustomDebugStringConvertible {
         /// Resource's url.
         public let source: URL
+        
+        /// Request id.
+        public let identifier: String
 
         /// Optional destination for the resource.
         public var destination: Destination?
-        
-        /// <#Description#>
-        public let kind: Kind
+
+        /// Flag that indicates if the request should be kept alive;
+        /// Taking advantage of URLSession's daemon.
+        public var keepAlive: Bool
+
+        /// Flag that indicates if the request should proceed anonymously.
+        public var shouldPersist: Bool
 
         /// A Boolean flag that determines whether connections should be made over a cellular network.
         public var allowsCellularAccess: Bool
@@ -48,23 +28,29 @@ extension Downloader {
         public var waitsForConnectivity: Bool
 
         public init(
-            source: URL,
-            destination: Destination? = nil,
-            kind: Kind = .default,
+            _ source: URL,
+            identifier: String? = nil,
+            destination: Destination?,
+            keepAlive: Bool,
+            shouldPersist: Bool,
             allowsCellularAccess: Bool,
             waitsForConnectivity: Bool
         ) {
             self.source = source
+            self.identifier = identifier ?? source.absoluteString
             self.destination = destination
-            self.kind = kind
+            self.keepAlive = keepAlive
+            self.shouldPersist = shouldPersist
             self.allowsCellularAccess = allowsCellularAccess
             self.waitsForConnectivity = waitsForConnectivity
         }
 
         public init?(
             _ urlString: String,
+            identifier: String? = nil,
             destination: Destination?,
-            kind: Kind,
+            keepAlive: Bool,
+            shouldPersist: Bool,
             allowsCellularAccess: Bool,
             waitsForConnectivity: Bool
         ) {
@@ -73,17 +59,20 @@ extension Downloader {
             }
 
             self.source = url
+            self.identifier = identifier ?? urlString
             self.destination = destination
-            self.kind = kind
+            self.keepAlive = keepAlive
+            self.shouldPersist = shouldPersist
             self.allowsCellularAccess = allowsCellularAccess
             self.waitsForConnectivity = waitsForConnectivity
         }
 
         public static func `default`(_ source: URL, destination: Destination? = nil) -> Request {
             Request(
-                source: source,
+                source,
                 destination: destination,
-                kind: .default,
+                keepAlive: true,
+                shouldPersist: true,
                 allowsCellularAccess: false,
                 waitsForConnectivity: true
             )
@@ -91,9 +80,10 @@ extension Downloader {
 
         public static func lowPriority(_ source: URL, destination: Destination? = nil) -> Request {
             Request(
-                source: source,
+                source,
                 destination: destination,
-                kind: .default,
+                keepAlive: true,
+                shouldPersist: false,
                 allowsCellularAccess: false,
                 waitsForConnectivity: true
             )
@@ -101,6 +91,14 @@ extension Downloader {
 
         func makeURLRequest() -> URLRequest {
             URLRequest(url: source)
+        }
+
+        public var description: String {
+            source.absoluteString
+        }
+
+        public var debugDescription: String {
+            source.absoluteString
         }
     }
 }
