6ad7b8afc53742557d1aaf44dd83f4f41df6a556
Sajad Vishkai <sajaclvishkai@gmail.com>
2025-07-02 15:17:30 +0330

    Update DownloaderTests.swift

--- Downloader/Tests/DownloaderTests/DownloaderTests.swift
+++ Downloader/Tests/DownloaderTests/DownloaderTests.swift
@@ -2,34 +2,74 @@ import XCTest
 @testable import Downloader
 
 final class DownloaderTests: XCTestCase {
-    func testExample() throws {
+    func testSingleFile() throws {
+        let manager = DownloadManager(name: "TEST")
+
+        let url: URL = try XCTUnwrap(
+            URL(string: "https://sample-files.com/downloads/documents/txt/long-doc.txt")
+        )
+
+        let request = Downloader.Request(
+            url,
+            destination: Downloader.Destination(fullPath: URL(string: "Sajad/Downloads/simple.txt")!),
+            keepAlive: false,
+            shouldPersist: true,
+            allowsCellularAccess: false,
+            waitsForConnectivity: true
+        )
+
+        manager.register(request)
+
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//            manager.pauseAll()
+//
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//                manager.resumeAll()
+//            }
+//        }
+
+        let promise = expectation(description: "")
+
+        wait(for: [promise], timeout: 10)
+    }
+
+    func testBunch() throws {
         let manager = DownloadManager(name: "TEST")
 
         let URLs: [URL] = try {
             [
-            try XCTUnwrap(
-                URL(string: "https://sample-files.com/downloads/documents/txt/simple.txt")
-            ),
-            try XCTUnwrap(
-                URL(string: "https://sample-files.com/downloads/documents/txt/long-doc.txt")
-            ),
+                try XCTUnwrap(
+                    URL(string: "https://sample-files.com/downloads/documents/txt/simple.txt")
+                ),
+                try XCTUnwrap(
+                    URL(string: "https://sample-files.com/downloads/documents/txt/long-doc.txt")
+                ),
 
             ]
         }()
 
-        let requests = URLs.map { Downloader.Request.default($0) }
+        let requests = URLs.map {
+            Downloader.Request(
+                $0,
+                destination: nil,
+                keepAlive: false,
+                shouldPersist: true,
+                allowsCellularAccess: false,
+                waitsForConnectivity: true
+            )
+        }
 
         requests.forEach { request in
             manager.register(request)
         }
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            manager.pauseAll()
-
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                manager.resumeAll()
-            }
-        }
+        //        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+        //            manager.pauseAll()
+        //
+        //            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+        //                manager.resumeAll()
+        //            }
+        //        }
 
         let promise = expectation(description: "")
 
